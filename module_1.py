81d4f240fdddf3ddd67bd384dfdcc08220099bf9
haidangcs14 <haidang9ahv1@gmail.com>
2023-12-26 12:42:26 +0700

    init

---
+++ module_1.py
@@ -0,0 +1,21 @@
+
+# Lesson 1: Hello world
+
+print("hello world.")
+
+# Lesson 2: Variable declaration
+
+variable = "hello world."
+print(variable)
+
+# Lesson 3: Expression
+
+variable_1 = "hello"
+variable_2 = "world."
+print(variable_1 + " " + variable_2)
+
+# Lesson 3: Function
+def print_result(val1, val2):
+    print(val1 + " " + val2)
+
+print_result(variable_1, variable_2)
